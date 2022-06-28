f4a5aa769eaab865dc800e9c23bd04c97a096725
Oscar Zhu <oscar@yvrmail.net>
2022-06-28 01:53:43 -0700

    added smart contract, created deployment unit tests

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,60 +1,65 @@
 const hre = require("hardhat");
-const { sendWebhook } = require('./util.js');
+const { sendWebhook } = require("./util.js");
 
 const parseArgs = () => {
-    let arguments = [];
-    require('../arguments.js').forEach(arg => {
-        arguments.push(arg)
-    })
-    return arguments;
+  let arguments = [];
+  require("../arguments.js").forEach((arg) => {
+    arguments.push(arg);
+  });
+  return arguments;
 };
 
 const getCurrentGas = async () => {
-    return hre.ethers.utils.formatUnits(
-        await hre.ethers.provider.getGasPrice(),
-        "gwei"
-    )
+  return hre.ethers.utils.formatUnits(
+    await hre.ethers.provider.getGasPrice(),
+    "gwei"
+  );
 };
 
-
 async function main() {
-    const [deployer] = await hre.ethers.getSigners();
+  const [deployer] = await hre.ethers.getSigners();
 
-    console.log("Deploying contracts with the account:", deployer.address);
-    console.log("Account balance:", hre.ethers.utils.formatEther(await deployer.getBalance()).toString(), "ETH");
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log(
+    "Account balance:",
+    hre.ethers.utils.formatEther(await deployer.getBalance()).toString(),
+    "ETH"
+  );
 
-    const ContractFactory = await hre.ethers.getContractFactory("NFT");    
+  const ContractFactory = await hre.ethers.getContractFactory("NFT");
 
-    const contract = await ContractFactory.deploy();
-    
-    await contract.deployed()    
+  const contract = await ContractFactory.deploy();
 
-    const gasFees = await hre.ethers.provider.getFeeData();
-    const receipt = await contract.deployTransaction.wait();
-    const gasUsed = receipt.gasUsed.toString();
+  await contract.deployed();
 
-    const txFee = Number(gasUsed) * (gasFees.maxFeePerGas !== null 
-        ? hre.ethers.utils.formatUnits(gasFees.maxFeePerGas, "gwei")
-        : hre.ethers.utils.formatUnits(gasFees.gasPrice, "ether"));
+  const gasFees = await hre.ethers.provider.getFeeData();
+  const receipt = await contract.deployTransaction.wait();
+  const gasUsed = receipt.gasUsed.toString();
 
-    let ctx = {
-        contract: contract.deployTransaction.creates,
-        hash: contract.deployTransaction.hash,
-        txFee: txFee.toString(),
-        gasLimit: contract.deployTransaction.gasLimit.toString(),
-        gasUsed: gasUsed,
-        chainId: contract.deployTransaction.chainId,
-    };
+  const txFee =
+    Number(gasUsed) *
+    (gasFees.maxFeePerGas !== null
+      ? hre.ethers.utils.formatUnits(gasFees.maxFeePerGas, "gwei")
+      : hre.ethers.utils.formatUnits(gasFees.gasPrice, "ether"));
 
-    console.log(ctx);
+  let ctx = {
+    contract: contract.deployTransaction.creates,
+    hash: contract.deployTransaction.hash,
+    txFee: txFee.toString(),
+    gasLimit: contract.deployTransaction.gasLimit.toString(),
+    gasUsed: gasUsed,
+    chainId: contract.deployTransaction.chainId,
+  };
 
-    await sendWebhook(ctx);
+  console.log(ctx);
+
+  await sendWebhook(ctx);
 }
 
 main()
-    .then(() => process.exit(0))
-    .catch((e) => {
-        console.error(e)
-        console.error(e.code);
-        process.exit(1);
-});
+  .then(() => process.exit(0))
+  .catch((e) => {
+    console.error(e);
+    console.error(e.code);
+    process.exit(1);
+  });
